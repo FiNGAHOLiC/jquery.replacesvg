b93033e1e87db72162c0cc60516b33fb1671dff2
FiNGAHOLiC <pl4y4h4ter@gmail.com>
2013-05-07 23:44:10 +0900

    create gh-pages

--- js/jquery.replacesvg.js
+++ js/jquery.replacesvg.js
@@ -3,7 +3,7 @@
  *
  * @modified  2013/05/07
  * @requires  jQuery 1.7.x or later
- * @version   1.0.2
+ * @version   1.0.3
  * @author    FiNGAHOLiC
  * @link      https://github.com/FiNGAHOLiC/jquery.replacesvg
  * @license   The MIT License
