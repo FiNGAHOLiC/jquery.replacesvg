5c5bb08b3b527d3af320a3bdc5a8c742922b11dc
FiNGAHOLiC <pl4y4h4ter@gmail.com>
2013-05-07 23:28:12 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# jquery.replacesvg.js
+
+jquery.replacesvg.js is a simple svg replacement plugin.
+
+## Demo
+
+http://fingaholic.github.com/jquery.replacesvg/
+
+## Usage
+
+### Options
+
+* **fallback** `Boolean` Base img is svg, set to true. If not so, set to false. (default value is false)
+* **dataname** `String` Data attribute name for getting alternative image path (default value is 'altimgsrc').
+* **ns** `String` Specifies the namespace URI (default value is 'http://www.w3.org/2000/svg').
+
+### Code
+
+```html
+<img src="./img/star.svg" width="100" height="95" alt="star" data-altimgsrc="./img/star.png" class="js-replacesvg">
+```
+
+```javascript
+$('.js-replacesvg').replacesvg();
+```
+
+## Requirements
+
+jQuery 1.7.x or later.
+
+## Browsers
+
+IE6+ and other new browsers.
+
+## License
+
+Licensed under the MIT license.
---
+++ js/jquery.replacesvg.js
@@ -0,0 +1,41 @@
+/*!
+ * jquery.replacesvg.js
+ *
+ * @modified  2013/05/07
+ * @requires  jQuery 1.7.x or later
+ * @version   1.0.0
+ * @author    FiNGAHOLiC
+ * @link      https://github.com/FiNGAHOLiC/jquery.replacesvg
+ * @license   The MIT License
+ *
+ */
+
+;(function($, window, document, undefined){
+
+	$.fn.replacesvg = function(options){
+		var o = $.extend({
+			fallback: false,
+			dataname: 'altimgsrc',
+			ns: 'http://www.w3.org/2000/svg'
+		}, options),
+		f = (function(){
+			var bln = (
+				!!document.createElementNS &&
+				!!document.createElementNS(o.ns, 'svg').createSVGRect
+			);
+			return function(){ return bln; };
+		}()),
+		support = f();
+		if(
+			!support && o.fallback ||
+			support && !o.fallback
+		){
+			return this.each(function(){
+				var dataname = $(this).data(o.dataname);
+				if(dataname) $(this).attr('src', dataname);
+			});
+		};
+
+	};
+
+})(jQuery, window, this.document);
